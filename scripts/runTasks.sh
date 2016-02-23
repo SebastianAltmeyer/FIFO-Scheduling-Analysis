1521a02c2a77d29922d0ce8ba43453e6a3c07945
Sebastian <sebastian.altmeyer@uni.lu>
2016-02-23 01:54:58 +0100

    Task generation script corrected

--- scripts/runTasks.sh
+++ scripts/runTasks.sh
@@ -2,34 +2,15 @@
 
 DEFAULTTASKSETS="1000"
 
-# for PERIODTYPE in "random" "lHarmonic" "harmonic" 
-# do
-# 	WENAME=../results/res_$PERIODTYPE"_tasks_we"
-# 
-# 	echo ""  > $WENAME 
-# 	
-# 	for TASKS in 2 4 6 8 10 12 16 20
-# 	do
-# 		../bin/rtAnalyser -def config -taskSets $DEFAULTTASKSETS -nrOfTasks $TASKS -periodType $PERIODTYPE -opt 1000  -o ../tmp/res_$PERIODTYPE"_"$TASKS 
-# 
-# 		FILENAME=../tmp/res_$PERIODTYPE"_"$TASKS"_we"
-# 		TEMPFile=../tmp/tmp
-# 		
-# 		echo $TASKS  > $TEMPFile 
-# 		paste $TEMPFile $FILENAME >> $WENAME
-# 		rm $TEMPFile
-# 	done
-# done
-
-for PERIODTYPE in "harmonic" 
+for PERIODTYPE in "random" "lHarmonic" "harmonic" 
 do
 	WENAME=../results/res_$PERIODTYPE"_tasks_we"
 
 	echo ""  > $WENAME 
 	
-	for TASKS in 2 4 6 8 10 12 16 20
+	for TASKS in 2 4 6 8 10 12 14 16 18 20
 	do
-		../bin/rtAnalyser -def config -taskSets $DEFAULTTASKSETS -nrOfTasks $TASKS -periodType $PERIODTYPE -opt 1000  -o ../tmp/res_$PERIODTYPE"_"$TASKS -oom 6
+		../bin/rtAnalyser -def config -taskSets $DEFAULTTASKSETS -nrOfTasks $TASKS -periodType $PERIODTYPE -opt 1000  -o ../tmp/res_$PERIODTYPE"_"$TASKS 
 
 		FILENAME=../tmp/res_$PERIODTYPE"_"$TASKS"_we"
 		TEMPFile=../tmp/tmp
@@ -39,5 +20,6 @@ do
 		rm $TEMPFile
 	done
 done
+
 
 echo "Done"
